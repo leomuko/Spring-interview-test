545fdc03ae193237a9ec0f1ed184666c2209ab1f
leomuko <kezzieleo@gmail.com>
2026-03-19 07:39:15 +0300

    Implement AuthController, add register user api logic

---
+++ src/main/java/com/operata/auth_service/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.operata.auth_service.controller;
+
+import com.operata.auth_service.dto.RegisterRequest;
+import com.operata.auth_service.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private  final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody RegisterRequest registerRequest) {
+        try {
+            String response = authService.registerUser(registerRequest);
+            return ResponseEntity.ok(response);
+        }catch (Exception ex){
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        }
+
+    }
+}
