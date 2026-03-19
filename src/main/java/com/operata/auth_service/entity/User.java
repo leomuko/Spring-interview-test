17b9f7db783201f80ef60976d058144302e13b41
leomuko <kezzieleo@gmail.com>
2026-03-19 07:40:26 +0300

    Implement User Model

---
+++ src/main/java/com/operata/auth_service/entity/User.java
@@ -0,0 +1,63 @@
+package com.operata.auth_service.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name= "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    public User() {}
+
+    public User(String phoneNumber, String email, String password) {
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+}
