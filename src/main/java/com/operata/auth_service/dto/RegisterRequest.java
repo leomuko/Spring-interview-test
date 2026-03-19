a4a1d88263d772bfb2fb13b57b3788ed9dbb2294
leomuko <kezzieleo@gmail.com>
2026-03-19 07:39:50 +0300

    Implement RegisterRequest Dto

---
+++ src/main/java/com/operata/auth_service/dto/RegisterRequest.java
@@ -0,0 +1,15 @@
+package com.operata.auth_service.dto;
+
+public class RegisterRequest {
+    private String phoneNumber;
+    private String email;
+    private String password;
+
+    // Getters and Setters
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
