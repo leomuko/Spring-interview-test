54fe331e24c2daaa9081fa7585b2000244c86251
leomuko <kezzieleo@gmail.com>
2026-03-19 07:40:55 +0300

    Implement the user repository

---
+++ src/main/java/com/operata/auth_service/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.operata.auth_service.repository;
+
+import com.operata.auth_service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findByPhoneNumber(String phoneNumber);
+}
