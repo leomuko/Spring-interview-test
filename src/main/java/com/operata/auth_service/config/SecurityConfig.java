f409a19b302e5f4fcf8c6b86b0b144e4fcb1e596
leomuko <kezzieleo@gmail.com>
2026-03-19 07:38:10 +0300

    Implement SecurityConfig file

---
+++ src/main/java/com/operata/auth_service/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.operata.auth_service.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF for REST APIs
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll() // Open up all /api/auth/ endpoints
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+
+}
