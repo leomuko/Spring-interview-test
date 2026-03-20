af1b3001c6b8cb7b69cd9876d4b1a8e4273a8844
leomuko <kezzieleo@gmail.com>
2026-03-20 10:57:20 +0300

    Implement Payment Db entity

---
+++ payment-service/src/main/java/com/operata/payment_service/entity/Payment.java
@@ -0,0 +1,64 @@
+package com.operata.payment_service.entity;
+
+import com.operata.payment_service.enums.PaymentStatus;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "payments")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //The unique ID we generate and gice to the user
+    @Column(nullable = false, unique = true)
+    private String referenceId;
+
+    // We store the user's email from the JWT to link the payment to them
+    @Column(nullable = false)
+    private String userEmail;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Column(nullable = false)
+    private String currency;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentStatus status;
+
+    // Shall use this when designing webhooks
+    @Column(nullable = true)
+    private String idempotencyKey;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    // Automatically set the creation time before saving
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getReferenceId() { return referenceId; }
+    public void setReferenceId(String referenceId) { this.referenceId = referenceId; }
+    public String getUserEmail() { return userEmail; }
+    public void setUserEmail(String userEmail) { this.userEmail = userEmail; }
+    public BigDecimal getAmount() { return amount; }
+    public void setAmount(BigDecimal amount) { this.amount = amount; }
+    public String getCurrency() { return currency; }
+    public void setCurrency(String currency) { this.currency = currency; }
+    public PaymentStatus getStatus() { return status; }
+    public void setStatus(PaymentStatus status) { this.status = status; }
+    public String getIdempotencyKey() { return idempotencyKey; }
+    public void setIdempotencyKey(String idempotencyKey) { this.idempotencyKey = idempotencyKey; }
+    public LocalDateTime getCreatedAt() { return createdAt; }
+
+}
